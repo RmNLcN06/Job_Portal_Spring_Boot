c2c4a2ebd8e1b9384da4fa1865f3c99f62b53725
RmNLcN06 <romain.luciano@orange.fr>
2025-03-10 11:01:07 +0100

    Initial commit

---
+++ Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/JobPortalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Job_Portal_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalSpringBootApplication.class, args);
+	}
+
+}
---
+++ Job_Portal_Spring_Boot/src/test/java/com/rmnlcn/Job_Portal_Spring_Boot/JobPortalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Job_Portal_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
