f77b98161ae0ee057336df28e92839388c928872
RmNLcN06 <romain.luciano@orange.fr>
2025-03-10 11:06:18 +0100

    Add Controllers package & HomeController class

---
+++ Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Job_Portal_Spring_Boot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
