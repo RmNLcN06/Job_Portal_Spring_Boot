b6372fb239980b3ee608a49c97f469e8ec5f307a
RmNLcN06 <romain.luciano@orange.fr>
2025-03-12 17:23:11 +0100

    Create Users controller

--- Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/services/UsersTypeService.java
+++ Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/services/UsersTypeService.java
@@ -2,6 +2,7 @@ package com.rmnlcn.Job_Portal_Spring_Boot.services;
 
 import com.rmnlcn.Job_Portal_Spring_Boot.entities.UsersType;
 import com.rmnlcn.Job_Portal_Spring_Boot.repositories.UsersTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class UsersTypeService {
 
     private final UsersTypeRepository usersTypeRepository;
 
+    @Autowired
     public UsersTypeService(UsersTypeRepository usersTypeRepository) {
         this.usersTypeRepository = usersTypeRepository;
     }
