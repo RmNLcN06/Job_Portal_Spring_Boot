c0f4dac882e1087fd31164deb7c714e0bd853b92
RmNLcN06 <romain.luciano@orange.fr>
2025-03-13 12:23:18 +0100

    Create RecruiterProfile entity

---
+++ Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/entities/RecruiterProfile.java
@@ -0,0 +1,147 @@
+package com.rmnlcn.Job_Portal_Spring_Boot.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "jp_recruiter_profile")
+public class RecruiterProfile {
+
+    // fields
+    @Id
+    @Column(name = "r_p_fk_user_id")
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name="r_p_fk_user_id")
+    @MapsId
+    private Users userId;
+
+    @Column(name = "r_p_first_name")
+    private String firstName;
+
+    @Column(name = "r_p_last_name")
+    private String lastName;
+
+    @Column(name = "r_p_city")
+    private String city;
+
+    @Column(name = "r_p_state")
+    private String state;
+
+    @Column(name = "r_p_country")
+    private String country;
+
+    @Column(name = "r_p_company")
+    private String company;
+
+    @Column(nullable = true, name = "r_p_photo", length = 64)
+    private String profilePhoto;
+
+
+    // constructors
+    public RecruiterProfile() {}
+
+    public RecruiterProfile(int userAccountId, Users userId, String firstName, String lastName, String city, String state, String country, String company, String profilePhoto) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.company = company;
+        this.profilePhoto = profilePhoto;
+    }
+
+
+    // getters/setters
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+
+    // toString() method
+    @Override
+    public String toString() {
+        return "RecruiterProfile{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", company='" + company + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                '}';
+    }
+}
