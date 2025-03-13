59ef74f4845d181594aa3786717e30a8144bca13
RmNLcN06 <romain.luciano@orange.fr>
2025-03-13 09:58:26 +0100

    Add request mapping for /register/new

--- Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/controllers/UsersController.java
+++ Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/controllers/UsersController.java
@@ -3,10 +3,12 @@ package com.rmnlcn.Job_Portal_Spring_Boot.controllers;
 import com.rmnlcn.Job_Portal_Spring_Boot.entities.Users;
 import com.rmnlcn.Job_Portal_Spring_Boot.entities.UsersType;
 import com.rmnlcn.Job_Portal_Spring_Boot.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,6 +28,12 @@ public class UsersController {
         model.addAttribute("getAllUsersTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users) {
+        System.out.println("User: " + users);
+        return "dashboard";
     }
 
 }
