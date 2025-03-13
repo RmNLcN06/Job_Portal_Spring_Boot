e5b118277c65c14ee835f249fd15ea9a4bf3505d
RmNLcN06 <romain.luciano@orange.fr>
2025-03-13 10:47:11 +0100

    Fix bug : duplicate email by adding "find a user by email" method to repository & service

--- Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/repositories/UsersRepository.java
+++ Job_Portal_Spring_Boot/src/main/java/com/rmnlcn/Job_Portal_Spring_Boot/repositories/UsersRepository.java
@@ -4,6 +4,10 @@ import com.rmnlcn.Job_Portal_Spring_Boot.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    Optional<Users> findByEmail(String email);
 }
